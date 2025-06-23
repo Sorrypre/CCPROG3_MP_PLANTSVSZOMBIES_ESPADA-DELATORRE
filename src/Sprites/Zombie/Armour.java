f834f85409386993ccc95bb85bdbfaefd1a0f8cb
Sorrypre <jjlespada5@gmail.com>
2025-06-23 10:04:34 +0800

    finished zombie and armor class

---
+++ src/Sprites/Zombie/Armour.java
@@ -0,0 +1,26 @@
+package Sprites.Zombie;
+
+public class Armour {
+    public Armour(){
+        NAME = "No Armour";
+        toleranceBonus = 0;
+        movementBonus = 0;
+    }
+    public Armour(String n, int tB, int mB){
+        NAME = n;
+        toleranceBonus = tB;
+        movementBonus = mB;
+    }
+    public String getName(){
+        return NAME;
+    }
+    public int getToleranceBonus(){
+        return toleranceBonus;
+    }
+    public int getMovementBonus(){
+        return movementBonus;
+    }
+    private String NAME;
+    private int toleranceBonus;
+    private int movementBonus;
+}
